450337bb7f39a36505f503fc47911bad4ec84d1a
random <a@b.com>
2024-07-09 17:14:55 +0800

    删除无用配置文件

---
+++ 6.reload.bat
@@ -0,0 +1,12 @@
+netstat -aon | findstr "9090" > nul
+if %errorlevel% equ 0 (
+curl "http://127.0.0.1:9090/configs?force=true" ^
+  -X "PUT" ^
+  -H "Content-Type: application/json" ^
+  -H "Authorization: Bearer 666999" ^
+  --data-raw ^"^{^\^"path^\^": ^\^"^\^", ^\^"payload^\^": ^\^"^\^"^}^"
+
+  echo "UPDATE CONFIG SUCCESS"
+)
+
+timeout /t 3
